6dccf40175f150320e23e2aa9cffb6ba5ed8fb91
chtheolo <chtheolo@gmail.com>
2021-07-22 12:01:27 +0300

    Config file for our service.

---
+++ config/main.js
@@ -0,0 +1,16 @@
+//Configuration file
+
+var port = {
+    development: 3000
+};
+
+var database = {
+        development: ''
+}
+
+module.exports = {
+    development: {
+        port: port.development,
+        database: database.development
+    }
+}
