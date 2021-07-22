1812038792d54a998d11d3a8aa11f679f6e8657e
chtheolo <chtheolo@gmail.com>
2021-07-22 15:18:48 +0300

    Change the endpoint.

--- router.js
+++ router.js
@@ -2,15 +2,22 @@ const express = require('express');
 
 // Route Controllers
 const controllers = {
+    numbers: require('./numbers')
 }
 
 
 // Route Groups
 const routes = {
+    numbers: express.Router(),
     api: express.Router()
 }
 
 module.exports = function(app) {
+
+    // get decimal numbers from clients & convert them
+    routes.api.use('/convert', routes.numbers);
+    routes.numbers
+        .post('/decTohex', controllers.numbers.post);
 
     // set url for API group routes
     app.use('/', routes.api);
