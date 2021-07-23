bca68f001319df9f8e562601f22954369026674c
chtheolo <chtheolo@gmail.com>
2021-07-23 20:07:35 +0300

    The file now has only the environmet variables for the db.

--- config/main.js
+++ config/main.js
@@ -1,16 +1,17 @@
 //Configuration file
-
-var port = {
-    development: 3000
-};
-
-var database = {
-        development: ''
+// const dotenv = require('dotenv').config({path: '/home/chtheolo/DecToHex_service/.env'});
+const dotenv = require('dotenv').config();
+if (dotenv.error) {
+    console.log(dotenv.error);
 }
 
 module.exports = {
     development: {
-        port: port.development,
-        database: database.development
+        db_host: process.env.POSTGRES_HOST,
+        db_port: process.env.POSTGRES_PORT,
+        db_user: process.env.POSTGRES_USER,
+        db_password: process.env.POSTGRES_PASSWORD,
+        db_name: process.env.POSTGRES_DB,
+        port: 3000
     }
 }
