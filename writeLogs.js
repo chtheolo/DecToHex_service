4462d1234d93f7559a6cddac0d71c558dbffece0
chtheolo <chtheolo@gmail.com>
2021-07-23 20:15:15 +0300

    A function for writing logs into an output.log file.

---
+++ writeLogs.js
@@ -0,0 +1,14 @@
+const { createWriteStream } = require('fs');
+const util = require('util');
+
+const write_log_stream = createWriteStream('output.log', { flags: 'a' });
+
+/** This function writes our logs into out output log file. */
+exports.write = function(logs) {
+    write_log_stream.write(util.format('%s\n',new Date().toUTCString()));
+    write_log_stream.write(util.format('%s\n', logs));
+
+    write_log_stream.on("error", (error) => {
+        console.log(error);
+    });
+}
