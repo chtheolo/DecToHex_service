3a9820f375b59cf8546064b415c8612cf24ae2b7
chtheolo <chtheolo@gmail.com>
2021-07-23 20:11:17 +0300

    Add error checking.

--- numbers/index.js
+++ numbers/index.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const convert = require('../conversions/decTohex');
+const logs = require('../writeLogs');
+const db = require('../db/queries');
+
 
 /** ------- POST -------- */
 exports.post = async function(req, res) {
     await convert.Dec2Hex(req.body.decimal)
     .then(result => {
+        logs.write(result);
+        db.post(result)
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.log(error);
+        })
         return res.status(200).send({
-            hexadecimal: result
+            data: result
         });
     })
     .catch(error => {
+        logs.write(error);
         return res.status(402).send(error);
     })
 }
